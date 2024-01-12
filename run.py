c883563cec061f24afc31ee92f6ff2e6d8d5a2f2
Hayden Caldwell <hade_caldwell@msn.com>
2024-01-12 21:50:39 +0000

    Add functions to choose random word and display it

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from words import sea_themed_words
+
 
 def clear_terminal():
     """
@@ -38,10 +41,42 @@ def game_rules():
     print("\nRules of the Game:")
     print("1. Your first challenge is to guess a four-letter word.")
     print("2. Successfully guessing a word advances you to the next level.")
-    print("3. Each new level increases the word length by one letter, up to a maximum of 9 letters.")
-    print("4. Keep guessing words until you reach the maximum length or make too many incorrect guesses.")
-    print("5. The ship will stay afloat as long as you keep the words afloat in your mind!")
+    print("3. During each round, you will have 6 lives.")
+    print("4. Each new level increases the word length by one letter, up to a maximum of 9 letters.")
+    print("5. Keep guessing words until you reach the maximum length or make too many incorrect guesses.")
+    print("6. The ship will stay afloat as long as you keep the words afloat in your mind!")
 
     print("\nAre you ready to set sail on this wordplay adventure? Let's begin!\n")
 
     # main()
+
+def get_random_word(word_length):
+    filtered_words = [
+            word for word in sea_themed_words if len(word) == word_length]
+    return random.choice(filtered_words)
+
+
+def display_word(word, guessed letters):
+    return ''.join(letter if letter in guessed_letters
+                   else '_' for letter in word)
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+Main function calls:
+def main():
+    number_of_lives = 6
+    random_word = get_random_word()
+    play_game(random_word, number_of_lives)
+
+welcome_page()
+"""
